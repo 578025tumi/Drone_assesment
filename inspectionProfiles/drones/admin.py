ec1f8d49aea75fbef0a9dd0a07ab75f9be63db19
578025tumi <138103070+578025tumi@users.noreply.github.com>
2025-01-20 22:29:01 +0200

    Delete inspectionProfiles directory

--- inspectionProfiles/drones/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Drone, Medication,BatteryAuditLog
-
-admin.site.register(Drone)
-admin.site.register(Medication)
-admin.site.register(BatteryAuditLog)
